0213a11153729bde5f2c073c67c1f4cb5e00b83c
Sergio Camargo <sergio.camargo813@gmail.com>
2018-02-05 13:51:05 -0800

    pass view pointers to key handler

--- main.c
+++ main.c
@@ -1,26 +1,24 @@
 #include <mlx.h>
 #include "libft.h"
 #include "keys.h"
+#include "wolf3d.h"
 
-int key_handler(int key, void *param)
+int key_handler(int key, t_view *view)
 {
 	if (key == KEY_ESC)
 		ft_putendl("escape");
-	//mlx_pixel_put(param[0], param[1], 10, 10, 0xFFFFFF);
+	mlx_pixel_put(view->mlx, view->window, 10, 10, 0xFFFFFF);
 	return (0);
 }
 
 int main(void)
 {
-	void *mlx;
-	void *window;
+	t_view *view;
 
-	mlx = mlx_init();
-	//params[0] = mlx;
-	window = mlx_new_window(mlx, 500, 500, "Wolf3d");
-	//params[1] = window;
-	mlx_pixel_put(mlx, window, 250, 250, 0xFFFFFF);
-	mlx_key_hook(window, key_handler, (void *)0);
-	mlx_loop(mlx);
+	view->mlx = mlx_init();
+	view->window = mlx_new_window(view->mlx, 500, 500, "Wolf3d");
+	mlx_pixel_put(view->mlx, view->window, 250, 250, 0xFFFFFF);
+	mlx_key_hook(view->window, key_handler, view);
+	mlx_loop(view->mlx);
 	return (0);
 }
